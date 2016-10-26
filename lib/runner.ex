c92526d9f4bb2330c49a169dffae041301652415
Charles Bernasconi <charles.bernasconi@xing.com>
2016-10-26 22:28:44 +0200

    Added circle and diagonal rover

--- lib/runner.ex
+++ lib/runner.ex
@@ -21,15 +21,26 @@ defmodule MarsRovers.Runner do
       def on_wall_collision(_), do: ~w(R R)
     end
     defmodule RandomCommander do
-      def on_idle, do: [Enum.random(~w(L R M M M M M M M))]
+      def on_idle, do: [Enum.random(~w(L R M M M M M M M M M M M M M M M M M M M M M M M M M))]
+      def on_wall_collision(_), do: ~w(R)
+    end
+    defmodule CircleCommander do
+      def on_idle, do: List.duplicate("M", 50) ++ ["R"]
+      def on_wall_collision(_), do: ~w(R)
+    end
+    defmodule DiagonalCommander do
+      def on_idle, do: List.duplicate("M", 50) ++ ["R"] ++ List.duplicate("M", 50) ++ ["L"]
       def on_wall_collision(_), do: ~w(R)
     end
     rovers = [
       %Rover{id: 1, x: 10, y: 2, d: "N", commander: WallCommanderL},
       %Rover{id: 2, x: 3, y: 3, d: "E", commander: WallCommanderR},
       %Rover{id: 3, x: 8, y: 8, d: "W", commander: BounceCommander},
-      %Rover{id: 4, x: 5, y: 5, d: "W", commander: RandomCommander}
+      %Rover{id: 4, x: 5, y: 5, d: "W", commander: RandomCommander},
+      %Rover{id: 5, x: 100, y: 100, d: "W", commander: CircleCommander},
+      %Rover{id: 6, x: 100, y: 100, d: "W", commander: DiagonalCommander}
     ]
-    [_rover1, _rover2] = Setup.deploy_rovers(rovers, -1, delay)
+
+    Setup.deploy_rovers(rovers, -1, delay)
   end
 end
